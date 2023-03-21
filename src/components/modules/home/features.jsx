70e9116bf3e85691f577bc621cf0655542cca039
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-22 02:11:46 +0430

    create features and add kuraman svg

--- src/components/modules/home/features.jsx
+++ src/components/modules/home/features.jsx
@@ -1,13 +1,19 @@
 import React from "react";
-import { Box, Container, Flex, Heading } from "@chakra-ui/react";
+import { Box, Container, Flex, Heading, Image } from "@chakra-ui/react";
 
 const Features = () => {
   return (
-    <Container my={20} maxW="container.lg">
-      <Flex justifyContent="space-between"></Flex>
-      <Box>
-        <Heading>SENJU Fake anime API</Heading>
-      </Box>
+    <Container mt={10} maxW="container.lg">
+      <Flex justifyContent="space-between" alignItems="center">
+        <Box>
+          <Heading>
+            SENJU <br /> Fake anime API
+          </Heading>
+        </Box>
+        <Box>
+          <Image src="kurama.svg" alt="kurama" />
+        </Box>
+      </Flex>
     </Container>
   );
 };
