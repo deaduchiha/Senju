8b8625b0745b8a98428004e4ce5985b3bd0271e0
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-22 01:28:02 +0430

    create button for navbar

--- src/components/modules/common/navbar.jsx
+++ src/components/modules/common/navbar.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import Image from "next/image";
-import { Box, Container, Flex } from "@chakra-ui/react";
+import { Button, Container, Flex } from "@chakra-ui/react";
 
 const Navbar = () => {
+  const nav = ["home", "docs", "github", "buy me a coffee"];
   return (
     <Container maxW="container.lg">
       <Flex py={2} justifyContent="space-between" alignItems="center">
         <Image width={100} height={20} src="senju.svg" alt="" />
-        <Box>salam</Box>
+        <Flex gap={5}>
+          {nav.map((item, index) => (
+            <Button key={index}>{item}</Button>
+          ))}
+        </Flex>
       </Flex>
     </Container>
   );
