87e2a62f16615691dfc48b4d6e280746c71fd532
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-22 04:44:31 +0430

    clean code

--- src/components/templates/home/landing.tsx
+++ src/components/templates/home/landing.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 // components
-import Navbar from "@/components/modules/common/navbar";
 import Features from "@/components/modules/home/features";
 
 const Landing = () => {
