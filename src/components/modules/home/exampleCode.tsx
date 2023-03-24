041c42464869971c6dc44b3b9ba52a2a80b2e58b
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-24 22:55:12 +0330

    edit custom hook

--- src/components/modules/home/exampleCode.tsx
+++ src/components/modules/home/exampleCode.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Box, Heading } from "@chakra-ui/react";
+import useCodeBlock from "@/hooks/codeBlock";
 
-import CodeBlock from "@/constant/prism/codeBlock";
 import { exampleCode } from "@/constant/prism/code";
 
 const ExampleCode = () => {
+  const highlightedCode = useCodeBlock("javascript", exampleCode);
   return (
     <Box my={10}>
       <Heading as="h3">Example Code</Heading>
-      <CodeBlock language="javascript" code={exampleCode} />
+      {highlightedCode}
     </Box>
   );
 };
