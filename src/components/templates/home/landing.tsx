dc86fb0b61d6919121101fee53e8752db251b84b
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-24 16:57:07 +0330

    add description into landing template

--- src/components/templates/home/landing.tsx
+++ src/components/templates/home/landing.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 // components
 import Features from "@/modules/home/features";
+import Description from "@/modules/home/description";
 
 const Landing = () => {
   return (
     <>
       <Features />
+      <Description />
     </>
   );
 };
