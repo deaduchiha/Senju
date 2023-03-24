39beab9efc14482efff7c32a609bb8b2c7adfd6a
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-24 22:39:46 +0330

    change address

--- src/components/templates/home/landing.tsx
+++ src/components/templates/home/landing.tsx
@@ -2,7 +2,7 @@ import React from "react";
 // components
 import Features from "@/modules/home/features";
 import Description from "@/modules/home/description";
-import ExampleCode from "@/components/modules/home/exampleCode";
+import ExampleCode from "@/modules/home/exampleCode";
 
 const Landing = () => {
   return (
