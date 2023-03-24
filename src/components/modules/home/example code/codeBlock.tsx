ff9d5b1c7613725d1dc8df600dbc97bebe8d9ff2
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-24 21:18:23 +0330

    change prismjs theme

--- src/components/modules/home/example code/codeBlock.tsx
+++ src/components/modules/home/example code/codeBlock.tsx
@@ -2,6 +2,7 @@ import Prism from "prismjs";
 import "prismjs/components/prism-javascript";
 import "prismjs/components/prism-css";
 import "prismjs/components/prism-jsx";
+import "prismjs/themes/prism-tomorrow.css"; // Import the theme CSS file
 
 interface CodeBlockProps {
   language: string;
@@ -12,8 +13,11 @@ const CodeBlock: React.FC<CodeBlockProps> = ({ language, code }) => {
   const html = Prism.highlight(code, Prism.languages[language], language);
 
   return (
-    <pre>
-      <code dangerouslySetInnerHTML={{ __html: html }} />
+    <pre className={`language-${language}`}>
+      <code
+        className={`language-${language}`}
+        dangerouslySetInnerHTML={{ __html: html }}
+      />
     </pre>
   );
 };
