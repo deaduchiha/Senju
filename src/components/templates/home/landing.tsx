7380aa09fd0cdf34385a9b7a72f77aad2ae1d661
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-24 21:04:48 +0330

    change example code path

--- src/components/templates/home/landing.tsx
+++ src/components/templates/home/landing.tsx
@@ -2,7 +2,7 @@ import React from "react";
 // components
 import Features from "@/modules/home/features";
 import Description from "@/modules/home/description";
-import ExampleCode from "@/modules/home/exampleCode";
+import ExampleCode from "@/modules/home/example code/exampleCode";
 
 const Landing = () => {
   return (
