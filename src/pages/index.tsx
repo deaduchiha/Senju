d0453f6397aaa6a17af259b0c86ac213f7fd0ce0
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-24 03:38:16 +0330

    add new title for homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
+import useTitle from "@/hooks/pageTitle";
+
 import Landing from "@/templates/home/landing";
 
 import React from "react";
 // components
 
 const Home = () => {
+  useTitle("Senju | Free fake anime API");
   return <Landing />;
 };
 
