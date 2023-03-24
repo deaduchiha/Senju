6d3836e757a3e2a64f71d41fd17a5ec4e2ae6031
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-24 21:10:47 +0330

    add new primjs package and add code block on example

--- src/components/modules/home/example code/exampleCode.tsx
+++ src/components/modules/home/example code/exampleCode.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { Box, Flex, Heading } from "@chakra-ui/react";
+import CodeBlock from "./codeBlock";
+import { Box, Heading } from "@chakra-ui/react";
 
 const ExampleCode = () => {
+  const code = `const greeting = "Hello, world!";
+  console.log(greeting);`;
   return (
     <Box my={10}>
       <Heading as="h3">Example Code</Heading>
+      <CodeBlock language="javascript" code={code} />
     </Box>
   );
 };
