d8257edcdb339ac5eaede9b4bfc9fc459fed8d85
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-26 21:54:06 +0330

    complete side bar and docs title

--- src/components/constant/docsTitle.ts
+++ src/components/constant/docsTitle.ts
@@ -1,10 +1,19 @@
 export const tutorial = [
-  "Get all animes",
-  "Get a single anime",
-  "Limit results",
-  "Get all characters",
-  "Add new anime",
-  "Add new characters",
-  "Update a characters",
-  "Delete a characters",
+  { type: "anime", name: "Get all animes", id: "Get-All-Anime" },
+  { type: "anime", name: "Get a single anime", id: "Get-A-Single-Anime" },
+  { type: "anime", name: "Get a single anime", id: "Get-A-Single-Anime" },
+  { type: "anime", name: "Limit results", id: "Limit-Results" },
+  { type: "characters", name: "Get all characters", id: "Get-All-Characters" },
+  { type: "anime", name: "Add new anime", id: "Add-New-Anime" },
+  { type: "characters", name: "Add new characters", id: "Add-New-Characters" },
+  {
+    type: "characters",
+    name: "Update a characters",
+    id: "Update-A-Characters",
+  },
+  {
+    type: "characters",
+    name: "Delete a characters",
+    id: "Delete-A-Characters",
+  },
 ];
--- src/components/modules/docs/Sidebar/menu.tsx
+++ src/components/modules/docs/Sidebar/menu.tsx
@@ -8,15 +8,45 @@ import { Link } from "react-scroll";
 const DocsMenu = () => {
   return (
     <Box>
-      <Heading as="h3" size="md">
+      <Heading as="h3" size="md" mb={3}>
         Anime
       </Heading>
-      <Flex flexDirection="column" gap={5}>
-        {tutorial.map((item, index) => (
-          <Link to={item} smooth={true} duration={500} key={index}>
-            {item}
-          </Link>
-        ))}
+      <Flex flexDirection="column" gap={3} pl={1}>
+        {tutorial.map((item, index) =>
+          item.type === "anime" ? (
+            <Link
+              to={item.id}
+              smooth={true}
+              duration={500}
+              key={index}
+              style={{ cursor: "pointer" }}
+            >
+              {item.name}
+            </Link>
+          ) : (
+            ""
+          )
+        )}
+      </Flex>
+      <Heading as="h3" size="md" mt={5} mb={3}>
+        Characters
+      </Heading>
+      <Flex flexDirection="column" gap={3} pl={1}>
+        {tutorial.map((item, index) =>
+          item.type === "characters" ? (
+            <Link
+              to={item.id}
+              smooth={true}
+              duration={500}
+              key={index}
+              style={{ cursor: "pointer" }}
+            >
+              {item.name}
+            </Link>
+          ) : (
+            ""
+          )
+        )}
       </Flex>
     </Box>
   );
