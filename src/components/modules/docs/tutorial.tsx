99edf2b0058f7be94bda6772dd76b6b0dba164a3
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-26 22:11:51 +0330

    complete tutorial

--- src/components/modules/docs/tutorial.tsx
+++ src/components/modules/docs/tutorial.tsx
@@ -1,15 +1,49 @@
 import { tutorial } from "@/components/constant/docsTitle";
-import { Flex, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Text } from "@chakra-ui/react";
+
+import useCodeBlock from "@/hooks/codeBlock";
+import { exampleCode } from "@/components/constant/code";
 
 const TutorialList = () => {
+  const highlightedCode = useCodeBlock("javascript", exampleCode);
+
   return (
     <>
+      <Heading mt={20} mb={5} color="#f49700" size="md">
+        Anime
+      </Heading>
       <Flex flexDirection="column" gap={20}>
-        {tutorial.map((item, index) => (
-          <Text key={index} id={item.id} mb={10}>
-            {item.name}
-          </Text>
-        ))}
+        {tutorial.map((item, index) =>
+          item.type === "anime" ? (
+            <Flex key={index} id={item.id} flexDirection="column" gap={2}>
+              <Text fontSize="larger">{item.name}</Text>
+              {highlightedCode}
+              <Button w="fit-content" colorScheme="docButton">
+                show output
+              </Button>
+            </Flex>
+          ) : (
+            ""
+          )
+        )}
+      </Flex>
+      <Heading mt={20} mb={5} color="#f49700" size="md">
+        Characters
+      </Heading>
+      <Flex flexDirection="column" gap={20}>
+        {tutorial.map((item, index) =>
+          item.type === "characters" ? (
+            <Flex key={index} id={item.id} flexDirection="column" gap={2}>
+              <Text fontSize="larger">{item.name}</Text>
+              {highlightedCode}
+              <Button w="fit-content" colorScheme="docButton">
+                show output
+              </Button>
+            </Flex>
+          ) : (
+            ""
+          )
+        )}
       </Flex>
     </>
   );
