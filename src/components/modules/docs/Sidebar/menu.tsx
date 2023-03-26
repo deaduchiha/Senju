e8df1350f2595a587335ad7ba844c41306e1df44
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-26 21:08:21 +0330

    install react scroll

--- src/components/modules/docs/Sidebar/menu.tsx
+++ src/components/modules/docs/Sidebar/menu.tsx
@@ -1,10 +1,11 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Heading, Text } from "@chakra-ui/react";
+import Link from "next/link";
 
 const DocsMenu = () => {
   return (
     <Box>
       <Heading as="h3" size="md">
-        Anime
+        <Text as={Link} href="#"></Text>
       </Heading>
     </Box>
   );
