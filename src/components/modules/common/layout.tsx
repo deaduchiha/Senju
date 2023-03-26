f9cf6e498d8528c26cb4963439885fbfebfd2bab
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-26 15:31:17 +0330

    edit container for docs

--- src/components/modules/common/layout.tsx
+++ src/components/modules/common/layout.tsx
@@ -1,5 +1,6 @@
 // react
 import React, { ReactNode } from "react";
+import { useRouter } from "next/router";
 // chakra ui
 import { Container } from "@chakra-ui/react";
 // components
@@ -11,10 +12,14 @@ type LayoutProps = {
 };
 
 const Layout = ({ children }: LayoutProps) => {
+  const router = useRouter();
+  const { route } = router;
+  console.log(route);
+
   return (
     <>
       <Navbar />
-      <Container mt={10} maxW="container.lg">
+      <Container mt={10} maxW={route === "/docs" ? "full" : "container.lg"}>
         {children}
       </Container>
       <Footer />
