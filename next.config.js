868fd59fdeab3ba516b6eb599d93b3ce058ad324
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-26 16:22:16 +0330

    redirect home to /

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  redirects: async () => {
+    return [{ source: "/home", destination: "/", permanent: true }];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
