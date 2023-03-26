a14718b380a359bae0d0e1323f8442a7ecdf6ebb
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-26 16:35:04 +0330

    use children for props

--- src/components/modules/docs/layout/docsLayout.tsx
+++ src/components/modules/docs/layout/docsLayout.tsx
@@ -1,17 +1,22 @@
 // react & next
-import React from "react";
+import React, { ReactNode } from "react";
 // chakra ui
 import { Box, Flex } from "@chakra-ui/react";
 // components
 import DocsNavbar from "./docsNav";
 import DocsSideBar from "./docsSideBar";
 
-const DocsLayout = () => {
+type LayoutProps = {
+  children: ReactNode;
+};
+
+const DocsLayout = ({ children }: LayoutProps) => {
   return (
     <Flex justifyContent="flex-start">
       <DocsSideBar />
       <Box w="full">
         <DocsNavbar />
+        {children}
       </Box>
     </Flex>
   );
