d9e5d2c202c23f32bddb7eefab35babd5c9e22e5
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-26 14:19:27 +0330

    create footer for layout

--- src/components/modules/common/footer.tsx
+++ src/components/modules/common/footer.tsx
@@ -1,3 +1,18 @@
-const Footer = () => {};
+import { Flex, Text } from "@chakra-ui/react";
+
+const Footer = () => {
+  return (
+    <Flex
+      mt={20}
+      h="100"
+      bg="#f5f5f5"
+      justifyContent="center"
+      alignItems="center"
+      fontSize="sm"
+    >
+      <Text>made with ❤️ by Alireza Nikzad & Vafa Nobakht</Text>
+    </Flex>
+  );
+};
 
 export default Footer;
