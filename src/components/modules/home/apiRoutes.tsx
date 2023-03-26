d8a9cd24cd07098c6c70bf9ad324940525d4a779
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-26 13:58:07 +0330

    create routes

--- src/components/modules/home/apiRoutes.tsx
+++ src/components/modules/home/apiRoutes.tsx
@@ -2,6 +2,19 @@ import { Box, Heading, Text } from "@chakra-ui/react";
 import React from "react";
 
 const ApiRoutes = () => {
+  type routesName = Map<string, string>;
+  const routes: routesName = new Map([
+    ["GET", "/anime"],
+    ["GET", "/anime/naruto"],
+    ["GET", "/characters"],
+    ["GET", "/characters/1"],
+    ["GET", "/characters?limit=10"],
+    ["POST", "/anime"],
+    ["DELETE", "/anime/naruto"],
+    ["PUT", "/characters/1"],
+    ["PATCH", "/characters/1"],
+    ["DELETE", "/characters/1"],
+  ]);
   return (
     <Box>
       <Heading as="h3" size="md">
