8ae87e57fe0c22964ad3a5796cc800b550c26771
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-26 21:38:22 +0330

    create docTitle component

---
+++ src/components/constant/docsTitle.ts
@@ -0,0 +1,10 @@
+export const tutorial = [
+  "Get all animes",
+  "Get a single anime",
+  "Limit results",
+  "Get all characters",
+  "Add new anime",
+  "Add new characters",
+  "Update a characters",
+  "Delete a characters",
+];
