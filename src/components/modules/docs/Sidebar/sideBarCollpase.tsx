5f4e4b0e18b362a6ff781b56e776928b4ed2fd6e
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-27 02:10:54 +0330

    create navbar collapse components

---
+++ src/components/modules/docs/Sidebar/sideBarCollpase.tsx
@@ -0,0 +1,5 @@
+const NavbarCollapse = () => {
+  return <h1>This is collapse</h1>;
+};
+
+export default NavbarCollapse;
--- src/components/modules/docs/layout/docsSideBar.tsx
+++ src/components/modules/docs/layout/docsSideBar.tsx
@@ -5,7 +5,7 @@ import DocsMenu from "../Sidebar/menu";
 
 const DocsSideBar = () => {
   return (
-    <Flex w="30vh" bg="#fafafa" pl={5} pt={4}>
+    <Flex w="30vw" bg="#fafafa" pl={5} pt={4} flexDirection="column">
       <DocsMenu />
     </Flex>
   );
