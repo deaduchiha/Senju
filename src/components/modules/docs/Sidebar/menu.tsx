03558a3e5383bec0a156210ba3444268f68a8714
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-26 21:02:25 +0330

    create menu for sidebar

---
+++ src/components/modules/docs/Sidebar/menu.tsx
@@ -0,0 +1,13 @@
+import { Box, Heading } from "@chakra-ui/react";
+
+const DocsMenu = () => {
+  return (
+    <Box>
+      <Heading as="h3" size="md">
+        Anime
+      </Heading>
+    </Box>
+  );
+};
+
+export default DocsMenu;
