2dfd9fe165f11975f156484c4325267ce983f15e
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-27 02:08:46 +0330

    refactor and add breakpoints to theme

--- src/components/modules/docs/layout/docsSideBar.tsx
+++ src/components/modules/docs/layout/docsSideBar.tsx
@@ -5,22 +5,7 @@ import DocsMenu from "../Sidebar/menu";
 
 const DocsSideBar = () => {
   return (
-    <Flex
-      pos="fixed"
-      top="0"
-      left="0"
-      w={{ base: "full", md: "xs" }}
-      h="full"
-      bg="#fafafa"
-      color="#111"
-      overflowX="hidden"
-      overflowY="auto"
-      pt="6"
-      pb="4"
-      pl="4"
-      pr="3"
-      display={{ base: "none", md: "flex" }}
-    >
+    <Flex w="30vh" bg="#fafafa" pl={5} pt={4}>
       <DocsMenu />
     </Flex>
   );
