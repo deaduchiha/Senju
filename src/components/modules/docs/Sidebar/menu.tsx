112c0bf1007ecfc268f127409e93afae47e7bf81
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-26 21:41:28 +0330

    map on tutorial

--- src/components/modules/docs/Sidebar/menu.tsx
+++ src/components/modules/docs/Sidebar/menu.tsx
@@ -1,5 +1,8 @@
+// chakra ui
 import { Box, Flex, Heading } from "@chakra-ui/react";
-
+// components
+import { tutorial } from "@/constant/docsTitle";
+// react scroll
 import { Link } from "react-scroll";
 
 const DocsMenu = () => {
@@ -8,7 +11,13 @@ const DocsMenu = () => {
       <Heading as="h3" size="md">
         Anime
       </Heading>
-      <Flex flexDirection="column" gap={5}></Flex>
+      <Flex flexDirection="column" gap={5}>
+        {tutorial.map((item, index) => (
+          <Link to={item} smooth={true} duration={500} key={index}>
+            {item}
+          </Link>
+        ))}
+      </Flex>
     </Box>
   );
 };
